7cb523697a57abb608de307615166f0c3a4796c3
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-21 17:28:39 +0200

    Init socket

--- Server/Server.py
+++ Server/Server.py
@@ -1,32 +1,22 @@
 import socket as sk
-# import time
+import sys
+#import time
 
 # UDP socket creation
-sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+try:    
+    sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+    # localhost current IP address (I guess), port 10000
+    server_address = ('localhost', 10000)
+    print ('\n\r starting up on %s port %s' % server_address)
+    # port associated to socket
+    sock.bind(server_address)
+    print("Successful binding. Waiting for Client now.")
+    #TODO blocking and timeout
+    #sk.setblocking(0)
+    #sk.settimeout(15)
+except sk.error:
+    print("Failed to create socket")
+    sys.exit()
 
-# associating socket with port
 
-# localhost current IP address (I guess), port 10000 
-server_address = ('localhost', 10000)
-
-print ('\n\r starting up on %s port %s' % server_address 'Server socket initialized')
-sock.bind(server_address)
-print("Successful binding. Waiting for Client now.")
-
-
-
-
-#while True:
-#    print('\n\r waiting to receive message...')
-#    data, address = sock.recvfrom(4096)
-
-#    print('received %s bytes from %s' % (len(data), address))
-#    print (data.decode('utf8'))
-    
-    
-#    if data:
-#        data1='Programmazione di Reti'
-#        time.sleep(2)
-#        sent = sock.sendto(data1.encode(), address)
-#        print ('sent %s bytes back to %s' % (sent, address))
 

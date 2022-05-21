e2f5e18d62987842d6af6a35c09fb4b044dc87d4
Cristina <cristina.zoccola01@gmail.com>
2022-05-21 17:21:43 +0200

    Added a control to check the socket creation

--- Client/Client.py
+++ Client/Client.py
@@ -1,7 +1,12 @@
 import socket as sk
+import sys
 
-# UDP socket creation
-sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
-
-# Defining the server address to send messages
-server_address = ('localhost', 10000)
+try:
+    # UDP socket creation
+    sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+    print("Client socket creation")
+    # Defining the server address to send messages
+    server_address = ('localhost', 10000)
+except sk.error:
+    print("Failed to create the client socket")
+    sys.exit

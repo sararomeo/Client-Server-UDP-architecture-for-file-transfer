44270b66f23a99e40cce264b4b9396dce7636337
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-22 11:16:34 +0200

    Implement list func

--- Server/Server.py
+++ Server/Server.py
@@ -2,7 +2,7 @@ import socket as sk
 import sys
 #import time
 
-# UDP socket creation
+# UDP socket creation at server's startup
 try:    
     sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
     # localhost current IP address (I guess), port 10000
@@ -18,13 +18,23 @@ except sk.error:
     print("Failed to create socket")
     sys.exit()
 
-# creates a list with all available file
+# creates a list with all available files
 def ServerList():
     print("Trying to get available files list..")
-    
+    # gets the list of all files and dir in path dir
+    F = os.listdir(path)
+    # filesList contains just files
+    filesList = []
+    for file in F:
+        filesList.append(file)
+    filesListStr = str(filesList)
+    filesListEn = filesListStr.encode('utf-8')
+    s.sendto(filesListEn, clientAddr)
+    print("List sent from Server")
+
 #L’invio del messaggio di risposta al comando get contenente il file richiesto,
 # se presente, od un opportuno messaggio di errore;
-def ServerGet():
+def ServerGet(g):
     print("Trying to get your file..")
 
 #La ricezione di un messaggio put contenente il file da caricare sul server e
@@ -42,12 +52,14 @@ while True:
     print('\n\r Waiting to receive message...')
     data, clientAddr = sk.recvfrom(4096)
     text = data.decode('utf8')
-    t = text.split()
-    #TODO : fix Invalid syntax (pyflakes E) error 
-    match t[0]:
+    t = text.split(' ')
+    command = t[0]
+    fileName = t[1]
+    #TODO : fix Invalid syntax (pyflakes E) error
+    match command:
         case "get":
             print("Going to get your file..")
-            ServerGet()
+            ServerGet(fileName)
         case "put":
             print("Going to put your file..")
             ServerPut()
@@ -58,7 +70,7 @@ while True:
             ServerExit()
         case _:
             print("Unknown input.")
-            
+        
 print("End of communication, closing program.")
 quit()
 

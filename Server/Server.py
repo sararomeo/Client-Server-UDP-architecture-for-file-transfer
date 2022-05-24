8b2011759e4c83f374d11a44412373088a573dd0
Cristina <cristina.zoccola01@gmail.com>
2022-05-24 14:21:56 +0200

    Now the server works

--- Server/Server.py
+++ Server/Server.py
@@ -7,7 +7,7 @@ BUFFER_SIZE=4096
 try:    
     sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
     server_address = ('localhost', 10000)
-    print ('\n\rStarting up on %s port %s' % server_address)
+    print ('\n\r starting up on %s port %s' % server_address)
     sock.bind(server_address)
     print("Successful binding. Waiting for Client now.")
 except sk.herror:
@@ -18,46 +18,18 @@ def SendMessageToClient(msg):
     msgEn = msg.encode('utf-8')
     sock.sendto(msgEn, clientAddr)
     print(f'Sent {msg} message to client')
-    
+
 def ReceiveMessageFromClient():
     data, server = sock.recvfrom(BUFFER_SIZE)
-    dataDec = data.decode('utf8')
-    print(dataDec)
+    #dataDec = data.decode('utf8')
+    #print(dataDec)
     return data
 
-def ReceiveFile():
-    f = open(t[1], "wb")
-    bytes = b''
-    data = True
-    sock.settimeout(2)
-    try:
-        while data:
-            data, server = sock.recvfrom(1024)
-            bytes += data
-    except sk.timeout:
-        sock.settimeout(2)
-    finally:
-        f.write(bytes)
-        f.close()
-
-def ServerList():
-    SendMessageToClient("Correct command, trying to get available files list..")
-    path = os.getcwd()
-    F = os.listdir(path)
-    filesList = []
-    for file in F:
-        filesList.append(file)
-    filesList.remove('Server.py')
-    filesListStr = str(filesList)
-    filesListEn = filesListStr.encode('utf-8')
-    sock.sendto(filesListEn, clientAddr)
-    SendMessageToClient("List sent from Server")
-
 def ServerGet(filename):
-    SendMessageToClient("Correct command, trying to get your file..")
+    print("Correct command, trying to get your file..")
     if os.path.isfile(filename):
         SendMessageToClient("File exists.")
-        SendMessageToClient(filename)
+        #SendMessageToClient(filename)
         f = open (filename, "rb") 
         data = f.read(1024)
         while data:
@@ -69,22 +41,46 @@ def ServerGet(filename):
         SendMessageToClient("Error: file doesn't exist.")
 
 def ServerPut():
-    SendMessageToClient("Correct command, trying to put your file..")
-    message = ReceiveMessageFromClient()
-    if "Error" in message.decode():
-        SendMessageToClient("Wrong file name, retry")
+    print("Correct command, trying to put your file..")
+    if "Error" in ReceiveMessageFromClient().decode():
+        SendMessageToClient("File not uploaded")
         return
-    ReceiveFile()
-    SendMessageToClient("File received.")
+    print("Starting the upload")
+    f = open(t[1], "wb")
+    bytes = b''
+    data = True
+    sock.settimeout(2)
+    try:
+        while data:
+            data, server = sock.recvfrom(1024)
+            bytes += data
+    except TimeoutError:
+        f.write(bytes)
+        SendMessageToClient("File uploaded")
+        f.close()
+
+def ServerList():
+    print("Correct command, trying to get available files list..")
+    path = os.getcwd()
+    F = os.listdir(path)
+    filesList = []
+    for file in F:
+        filesList.append(file)
+    filesList.remove('Server.py')
+    filesListStr = str(filesList)
+    filesListEn = filesListStr.encode('utf-8')
+    sock.sendto(filesListEn, clientAddr)
+    #SendMessageToClient("List sent from Server")
+    #ReceiveMessageFromClient()
 
 def ServerExit():
-    print("Server socket closed, not sending any message to Client.\n\n")
+    print("Server socket closed, not sending any message to Client.")
     sock.close()
     sys.exit(0)
 
 while True:
-    print('\n\r\tWaiting to receive message...')
     try:
+        print('\n\r Waiting to receive message...')
         data, clientAddr = sock.recvfrom(BUFFER_SIZE)
         text = data.decode('utf8')
         t = text.split(' ',1)
@@ -100,6 +96,5 @@ while True:
             ServerExit()
         else:
             SendMessageToClient("Unknown input.")
-    except Exception:
+    except TimeoutError:
         pass
-
